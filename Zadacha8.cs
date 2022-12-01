8dd28437a8279993f6e76cd206f976fe79687185
DmitryMazaev <118983899+DmitryMazaev@users.noreply.github.com>
2022-12-01 14:22:23 +0300

    Update Zadacha8.cs

--- Zadacha8.cs
+++ Zadacha8.cs
@@ -1,4 +1,4 @@
-﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
@@ -6,14 +6,26 @@
 Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine()!);
 int i = 2;
-if (N>=i)
-    while (i<=N)
+if (N > 0)
+    while (i <= N)
     {
-        Console.Write(i+" ");
-        i=i+2;
+        Console.Write(i + " ");
+        i = i + 2;
     }
-
-else
+else if (N < 0 && N % 2 == 0)
+    while (N <= -i)
+    {
+        Console.Write(N + " ");
+        N = N + 2;
+    }
+else if (N < 0 && N % 2 == -1)
 {
-    Console.Write("Проверить данные");
+    N = N + 1;
+    while (N <= -i)
+    {
+        Console.Write(N + " ");
+        N = N + 2;
+    }
 }
+else
+    Console.Write("Попробуйте еще раз");
